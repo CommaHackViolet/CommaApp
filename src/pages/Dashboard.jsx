1d01fd6b2f7fd6e465a904662f866364a0fc1ab8
Ebenezer Zergabachew <eben.zergaw@gmail.com>
2024-02-03 21:51:58 -0500

    check in styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,20 +10,18 @@ function Dashboard() {
 
   const {currentUser} = useAuth()
   const {currentCheckIn, addOrUpdateCheckIn} = useCheckIn()
-  
-  const [checkInData, setCheckInData] = useState({
-    birthControl: false,
-    mood: 'neutral',
-    journal: 'I am feeling okay today',
-  })
+
+  const [birthControl, setBirthControl] = useState(false)
+  const [mood, setMood] = useState(null)
+  const [journal, setJournal] = useState('')
 
   const handleCheckIn = async () => {
 
-    const payload = {
-      birthControl: false,
-      mood: 'sad',
+    const checkInData = {
+      birthControl,
+      mood,
+      journal,
       date: new Date(),
-      journal: 'I am feeling shit today but its okay',
       userID: currentUser.uid
     }
 
@@ -33,39 +31,65 @@ function Dashboard() {
   return (
     <div>
       <div className="w-1/3">
-        <h2 className="text-2xl font-semibold">did you take your birth control?</h2>
-        <button className="btn btn-block btn-primary mt-6">yes</button>
+        <h2 className="text-2xl font-semibold mb-8">did you take birth control?</h2>
+
+        <div className={`btn btn-block hover:bg-red-300 hover:text-black ${birthControl ? 'btn-primary' : 'btn-outline btn-ghost'}`}
+          onClick={() => {
+            setBirthControl(!birthControl)
+            console.log(birthControl)
+          }}>
+          {
+          birthControl ? 'yes' : 'no'}
+        </div>
 
         <div className='mt-14'>
           <h2 className="text-2xl font-semibold">how is your mood today?</h2>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-success" />
+            <input type="radio" name="radio-5" className="radio radio-success" 
+              value='fantastic'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">fantastic</span>
           </div>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-primary" />
+            <input type="radio" name="radio-5" className="radio radio-primary" 
+              value='good'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">good</span>
           </div>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-secondary" />
+            <input type="radio" name="radio-5" className="radio radio-secondary"
+              value='okay'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">okay</span>
           </div>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-neutral" />
+            <input type="radio" name="radio-5" className="radio radio-neutral" 
+              value='neutral'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">neutral</span>
           </div>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-accent" />
+            <input type="radio" name="radio-5" className="radio radio-accent" 
+              value='sad'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">sad</span>
           </div>
 
           <div className="flex items-center my-5">
-            <input type="radio" name="radio-5" className="radio radio-error" />
+            <input type="radio" name="radio-5" className="radio radio-error text-red-900" 
+              value='horrible'
+              onChange={(e) => setMood(e.target.value)}
+              />
             <span className="ml-2 font-semibold">horrible</span>
           </div>
 
@@ -73,7 +97,11 @@ function Dashboard() {
 
         <div className="mt-14"> 
           <h2 className="text-2xl font-semibold mb-6">whats been on your mind?</h2>
-          <textarea className="textarea textarea-accent w-full h-60" placeholder="let your thoughts flow"></textarea>
+          <textarea className="textarea textarea-accent w-full h-60" 
+            placeholder="let your thoughts flow"
+            value={journal}
+            onChange={(e) => setJournal(e.target.value)}
+            ></textarea>
         </div>
         {/* <button className="btn btn-primary btn-lg" onClick={handleCheckIn}>check in</button> */}
       </div>
